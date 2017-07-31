8ee15159934a7e31109059334db1804a7993c31c
Eric torre <etorregc@hotmail.es>
2017-07-31 11:21:53 +0200

    add test controller

---
+++ Src/Api/Tokiota.Raspberry.Api/Controllers/TestController.cs
@@ -0,0 +1,15 @@
+﻿namespace Tokiota.Raspberry.Api.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    [Route("api/[controller]")]
+    public class TestController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return this.Ok(new { message = "Test" });
+        }
+    }
+}
